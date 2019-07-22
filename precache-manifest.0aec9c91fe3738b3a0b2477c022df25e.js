4a6d35f9e4ccbb31af73b1bd754398b03df0ac05
Eze <emereuwaonueze@gmail.com>
2019-07-22 15:16:43 +0100

    Updates

---
+++ precache-manifest.0aec9c91fe3738b3a0b2477c022df25e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56301b7a00191d69e9f77144873a4f76",
+    "url": "/domm-project/index.html"
+  },
+  {
+    "revision": "c9b646ce7a03a6b53447",
+    "url": "/domm-project/static/css/2.4234f707.chunk.css"
+  },
+  {
+    "revision": "943d70dfec0af64c7f40",
+    "url": "/domm-project/static/css/main.e5f70087.chunk.css"
+  },
+  {
+    "revision": "c9b646ce7a03a6b53447",
+    "url": "/domm-project/static/js/2.0c1cc52d.chunk.js"
+  },
+  {
+    "revision": "943d70dfec0af64c7f40",
+    "url": "/domm-project/static/js/main.e852eaad.chunk.js"
+  },
+  {
+    "revision": "4ec058ca77659b18754f",
+    "url": "/domm-project/static/js/runtime~main.997af9ba.js"
+  },
+  {
+    "revision": "72b126725b1ae8feb32f97267866fc14",
+    "url": "/domm-project/static/media/banner.72b12672.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/domm-project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/domm-project/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/domm-project/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/domm-project/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/domm-project/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "44b9eacc657a1d6caa219120f1367175",
+    "url": "/domm-project/static/media/customers.44b9eacc.png"
+  },
+  {
+    "revision": "e75700b86c129844d8cccc2446211d6d",
+    "url": "/domm-project/static/media/download.e75700b8.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/domm-project/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/domm-project/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/domm-project/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/domm-project/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/domm-project/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/domm-project/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0c31c6a759c513bd9b28dfbaa34f9f51",
+    "url": "/domm-project/static/media/integrations.0c31c6a7.png"
+  },
+  {
+    "revision": "e17fee8e594416feb7942c68140a8655",
+    "url": "/domm-project/static/media/middle_aged_woman.e17fee8e.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/domm-project/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/domm-project/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/domm-project/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/domm-project/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/domm-project/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "039c8217aed741b5ac08dcb8e7ec9d94",
+    "url": "/domm-project/static/media/phone.039c8217.png"
+  }
+]);
